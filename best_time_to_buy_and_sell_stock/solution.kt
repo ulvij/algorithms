b9dd79ac1757b19b04c8c69e3bbfbe9f74f72472
Ulvi Jabbarli <ulvi.cabbarli.96@gmail.com>
2020-04-05 22:16:13 +0400

    optimization on bestTimeToBuyAndSellStock problem

--- best_time_to_buy_and_sell_stock/solution.kt
+++ best_time_to_buy_and_sell_stock/solution.kt
@@ -1,25 +1,12 @@
 class Solution {
-    fun maxProfit(prices: IntArray): Int {
-        if (prices.isEmpty()) return 0
-
-        if (prices.size == 1) return 0
-
-        if (prices.size == 2) {
-            if (prices[0] < prices[1]) {
-                return prices[1] - prices[0]
-            }
+    fun maxProfit(prices:IntArray):Int{
+        if(prices.isEmpty()) return 0
+        var maxProfit = 0
+        var minStock = prices[0]
+        for (i in 1 until prices.size){
+            maxProfit = maxProfit.coerceAtLeast(prices[i]-minStock)
+            minStock = minStock.coerceAtMost(prices[i])
         }
-
-        val maxProfArray = IntArray(prices.size)
-        maxProfArray[0] = 0
-        var min = prices[0]
-        maxProfArray[1] = if (prices[0] < prices[1]) prices[1] - prices[0] else 0
-        min = min.coerceAtMost(prices[1])
-        for (i in 2..prices.lastIndex){
-            min = min.coerceAtMost(prices[i])
-            val cProfit = if (min < prices[i]) prices[i] - min else 0
-            maxProfArray[i] = maxProfArray[i-1].coerceAtLeast(cProfit)
-        }
-        return maxProfArray.last()
+        return maxProfit
     }
 }
